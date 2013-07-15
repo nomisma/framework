fb9cf5f8cab0226d2c26c3b8ad77d32b4ef59518
ewg118 <ewg4xuva@gmail.com>
2013-07-15 12:54:08 +0000

    adding line for git add

--- script/commit_ids.sh
+++ script/commit_ids.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 OUTPUT="$(date +%Y-%m-%dT%T) - Automatic Update"
 cd /usr/local/projects/nomisma-ids
+#add new files
+git add *.txt
+#commit
 git commit -am "$OUTPUT"
 git push
 echo "Done."
