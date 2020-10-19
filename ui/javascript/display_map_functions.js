2ed4d91269af6d35e2ffc52cf7fcc776a0d142a2
Ethan Gruber <ewg4xuva@gmail.com>
2020-10-19 14:46:28 -0400

    switched to new mapbox tile layer for physical

--- ui/javascript/display_map_functions.js
+++ ui/javascript/display_map_functions.js
@@ -16,17 +16,12 @@ function initialize_map(id) {
 	var mapboxKey = $('#mapboxKey').text();
 	
 	//baselayers
-	var awmcterrain = L.tileLayer(
-	'https://api.tiles.mapbox.com/v4/isawnyu.map-knmctlkh/{z}/{x}/{y}.png?access_token=' + mapboxKey, {
-		attribution: 'Powered by <a href="http://leafletjs.com/">Leaflet</a> and <a href="https://www.mapbox.com/">Mapbox</a>. Map base by <a title="Ancient World Mapping Center (UNC-CH)" href="http://awmc.unc.edu">AWMC</a>, 2014 (cc-by-nc).',
-		maxZoom: 12
-	});
-	
-	/* Not added by default, only through user control action */
-	var terrain = L.tileLayer(
-	'https://api.tiles.mapbox.com/v4/isawnyu.map-p75u7mnj/{z}/{x}/{y}.png?access_token=' + mapboxKey, {
-		attribution: 'Powered by <a href="http://leafletjs.com/">Leaflet</a> and <a href="https://www.mapbox.com/">Mapbox</a>. Map base by <a title="Institute for the Study of the Ancient World (ISAW)" href="http://isaw.nyu.edu">ISAW</a>, 2014 (cc-by).'
-	});
+	var mb_physical = L.tileLayer(
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>', id: 'mapbox/outdoors-v11', maxZoom: 12, accessToken: mapboxKey
+    });
 	
 	var osm = L.tileLayer(
 	'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -55,7 +50,7 @@ function initialize_map(id) {
 		center: new L.LatLng(0, 0),
 		zoom: 4,
 		//layers:[awmcterrain, heatmapLayer]
-		layers:[imperium]
+		layers:[mb_physical]
 	});
 	
 	//add mintLayer from AJAX
@@ -83,8 +78,7 @@ function initialize_map(id) {
 	
 	//add controls
 	var baseMaps = {
-		"Ancient Terrain": awmcterrain,
-		"Modern Terrain": terrain,
+	   "Terrain and Streets": mb_physical,
 		"Modern Streets": osm,
 		"Imperium": imperium
 	};
